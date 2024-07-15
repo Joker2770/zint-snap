2edca958e9482155916fb5525ae4c86f122c1154
Joker2770 <1214220480@qq.com>
2024-07-15 15:03:26 +0800

    docs: CRLF -> LF :bug:

--- launcher.sh
+++ launcher.sh
@@ -6,7 +6,7 @@
 #########################################################################
 
 #/***************************************************************************
-# *   Copyright (C) 2022-2024 Jintao Yang                                        *
+# *   Copyright (C) 2022-2024 Jintao Yang                                   *
 # *                                                                         *
 # *   This program is free software: you can redistribute it and/or modify  *
 # *   it under the terms of the GNU General Public License as published by  *
